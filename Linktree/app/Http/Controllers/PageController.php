7cd8fd765a053185cd14b84dbbb213b3ba81650a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-03-23 11:33:58 -0300

    criando_o_view_da_pagina_'page.blade.php'_verificando_se_a_mesma_existe_no_sistema_e_enviando_informacoes_do_banco_de_dados_para_ela

--- Linktree/app/Http/Controllers/PageController.php
+++ Linktree/app/Http/Controllers/PageController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Page;       // puchando o model 'Page'
 
 class PageController extends Controller {
-    public function index() {
-        echo 'pagina';
+    public function index($slug) {
+        $page = Page::where('slug', $slug)-> first();       // pegando as informações da página, pegando o primeiro registro
+
+        if($page) {      //verificando se a página foi encontrada
+            $bg = '';
+
+            $links = [];
+
+            return view('page', [
+                'font_color'=> $page-> op_font_color,       // enviando informações para a 'page'
+                'profile_image'=> url('/media/uploads'). '/'. $page-> op_profile_image,
+                'title'=> $page-> op_title,
+                'description'=> $page-> op_description,
+                'fb_pixel'=> $page-> op_fb_pixel,
+                'bg'=> $bg,
+                'links'=> $links
+            ]);
+        } else {
+            return view('notfound');
+        }
     }
 }
